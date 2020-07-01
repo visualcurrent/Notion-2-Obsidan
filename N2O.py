65428a0de18c8bf6f0139d1b6e2af78460b222d1
goodsignal <gabe@visualcurrent.com>
2020-07-01 11:10:19 +0700

    Add files as new working code
    Conversion appears to fully work. Plan to make minor improvements from here.

---
+++ N2O.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 13:34:37 2020
+
+@author: books
+"""
+import os
+import re
+import shutil
+import zipfile
+from pathlib import Path
+import N2Omodule
+import tempfile
+import easygui
+
+
+NotionZip = Path(easygui.fileopenbox())
+
+
+# Load zip file
+notionsData = zipfile.ZipFile(NotionZip, 'r')
+
+NotionPathRaw = []
+ObsidianPathRaw = []
+NotionPaths = []
+ObsidianPaths = []
+
+
+
+# Generate a list of file paths for all zip content
+[NotionPathRaw.append(line.rstrip()) for line in notionsData.namelist()]
+# [print(l[0],l[1]) for l in enumerate(NotionPaths)]
+
+
+
+# Clean paths for Obsidian destination
+regexUID = "\s\w{32}"
+for line in NotionPathRaw:
+    ObsidianPathRaw.append(re.sub(regexUID, "", line))
+# [print(l[0],l[1]) for l in enumerate(ObsidianPaths)]
+
+
+### PATHS IN PROPER OS FORM BY PATHLIB ###
+[NotionPaths.append(Path(line)) for line in NotionPathRaw]
+[ObsidianPaths.append(Path(line)) for line in ObsidianPathRaw]
+
+
+
+# Get all the relevant indices (folders, .md, .csv, others)
+mdIndex, csvIndex, othersIndex, folderIndex, folderTree = N2Omodule.ObsIndex(ObsidianPaths)
+ 
+
+# Rename the .csv files to .md files for the conversion
+for i in csvIndex:
+    ObsidianPaths[i] = Path(str(ObsidianPaths[i])[0:-3]+"md")
+# [print(ObsidianPaths[i]) for i in csvIndex]
+
+
+## Create a temporary directory to work with
+unzipt = tempfile.TemporaryDirectory()
+tempPath = Path(unzipt.name)
+# print('tempPath',tempPath)
+
+
+## Create temp directory paths that match zip directory tree
+tempDirectories = []
+
+# Construct complete directory paths (<tempDirecory>/<zipDirectories>)
+for d in folderTree:
+    tempDirectories.append(tempPath / d)
+# [print(l) for l in tempDirectories]
+
+## Create the temporary directory structure for future archive
+for d in tempDirectories:
+    os.makedirs(d, exist_ok=True)
+# print(os.listdir(unzipt.name))
+
+
+
+
+
+
+# Process all CSV files
+for n in csvIndex:
+    
+    # Access the original CSV file
+    with notionsData.open(NotionPathRaw[n], "r") as csvFile:
+        # print("===",ObsidianPaths[n])
+         
+        # Convert CSV content into Obsidian Internal Links
+        mdTitle = N2Omodule.N2Ocsv(csvFile)
+    
+        ## Make temp destination file path
+        newfilepath = tempPath / ObsidianPaths[n]
+        
+        # Check if file exists, append if true
+        if os.path.exists(newfilepath):
+            append_write = 'a' # append if already exists
+        else:
+            append_write = 'w' # make a new file if not
+        
+        # Save CSV internal links as new .md file
+        with open(newfilepath, append_write) as tempFile:
+            [print(line.rstrip(), file=tempFile) for line in mdTitle]
+        
+    ### This shows the contents of the saved test file!    
+    # with open(newfilepath, "r") as f:
+    #     print("from saved tempfile:")
+    #     [print(line.rstrip()) for line in f]
+
+
+
+
+
+
+# Process all MD files
+for n in mdIndex:
+    
+    # Access the original MD file
+    with notionsData.open(NotionPathRaw[n], "r") as mdFile:
+        # [print(line.decode().rstrip()) for line in mdFile]
+        
+        # Find and convert Internal Links to Obsidian style
+        mdContent = N2Omodule.N2Omd(mdFile)
+        # [print(l)for l in mdContent]
+        
+        ## Make temp destination file path
+        newfilepath = tempPath / ObsidianPaths[n]
+        # print(newfilepath)
+        
+        # Check if file exists, append if true
+        if os.path.exists(newfilepath):
+            append_write = 'a' # append if already exists
+        else:
+            append_write = 'w' # make a new file if not
+        # print(append_write)
+        
+        # Save modified content as new .md file
+        with open(newfilepath, append_write, encoding='utf-8') as tempFile:
+            [print(line.rstrip(), file=tempFile) for line in mdContent]
+        
+
+
+    ### This check just shows the contents of the saved test file!    
+    # with open(newfilepath, "r", encoding='utf-8') as f:
+    #     print("from saved tempfile:")
+    #     [print(line.rstrip()) for line in f]
+
+
+
+
+
+
+#### Process all attachment files using othersIndex ####
+for n in othersIndex:
+    
+    # Move the file from NotionPathRaw[n] in zip to newfilepath = tempPath / ObsidianPaths[n]
+    newfilepath = tempPath / ObsidianPaths[n]
+    # print('newfilepath:', newfilepath)
+    
+    with notionsData.open(NotionPathRaw[n]) as zf:
+        with open(newfilepath, 'wb') as f:
+            shutil.copyfileobj(zf, f)
+
+    
+
+
+
+
+# Save temporary file collection to new zip
+shutil.make_archive( NotionZip.parent / (NotionZip.name[:-4]+'-ObsidianReady'), 'zip', tempPath)
+
+
+
+
+
+
+# Check files in destination
+# def tree2(directory):
+#     print(f'+ {directory}')
+#     for path in sorted(directory.rglob('*')):
+#         depth = len(path.relative_to(directory).parts)
+#         spacer = '    ' * depth
+#         print(f'{spacer}+ {path.name}')
+# tree2(tempPath)
+
+
+
+
+# Close out!
+notionsData.close()
---
+++ N2Omodule.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 14:16:18 2020
+
+@author: books
+"""
+
+import io
+import os
+import re
+import csv
+from pathlib import Path
+
+
+def ObsIndex(contents):
+    """
+    Function to return all the relevant indices 
+    Requires: contents are pre-conditioned by pathlib.Path()
+    Returns: (mdIndex, csvIndex, othersIndex, folderIndex, folderTree)
+    """
+    
+    ## index the directory structure
+    folderIndex = []
+    folderTree = []
+
+    for line in enumerate(contents):
+        if not line[1].suffix:
+            folderIndex.append(line[0]) #save index
+            folderTree.append(line[1])
+    ## Case: directories are implicit
+    if not folderIndex:
+        Tree = list(set([os.path.dirname(x) for x in contents]))
+        [folderTree.append(Path(l)) for l in Tree]
+
+    
+    ## Index the .md files
+    mdIndex = []
+    for line in enumerate(contents):
+        if line[1].suffix == ".md":
+            mdIndex.append(line[0]) #save index
+    # print("mdIndex",mdIndex)
+    
+    
+    ## Index the .csv files
+    csvIndex = []
+    for line in enumerate(contents):
+        if line[1].suffix == ".csv":
+            csvIndex.append(line[0]) #save index
+    # print("csvIndex",csvIndex)
+
+    
+    ## index the other files using set difference
+    othersIndex = list(set(range(0,len(contents)))
+        - (set(folderIndex)|set(mdIndex)|set(csvIndex)))
+    # [print(i, contents[i]) for i in othersIndex]
+    
+    return mdIndex, csvIndex, othersIndex, folderIndex, folderTree
+
+
+def N2Ocsv(csvFile):
+    
+    # Convert csv to dictionary object
+    reader = csv.DictReader(io.TextIOWrapper(csvFile, "utf-8-sig"), delimiter=',', quotechar='"')
+   
+    dictionry = {}
+    for row in reader: # I don't know how this works but it does what I want
+        for column, value in row.items():
+            dictionry.setdefault(column, []).append(value)
+           
+    IntLinks = list(dictionry.keys())[0] # Only want 1st column header    
+    oldTitle = dictionry.get(IntLinks)
+
+    Title = []
+    mdTitle = []
+
+    # Clean Internal Links
+    regexURLid = "(?:https?|ftp):\/\/"
+    regexSymbols = "[^\w\s]"
+    regexSpaces = "\s+"
+    for line in oldTitle:
+        line = line.rstrip()
+        #1 Replace URL identifiers and/or symbols with a space
+        line = re.sub(regexURLid," ",line)
+        line = re.sub(regexSymbols," ",line)
+         #2 Remove duplicate spaces
+        line = re.sub(regexSpaces, " ", line)        
+        #3 Remove any spaces at beginning
+        line = line.lstrip()
+        #4 Cut title at 50 characters
+        line = str(line)[0:50]
+        #5 Remove any spaces at end
+        line = line.rstrip()    
+        if line:
+            Title.append(line)
+    
+    ## convert Titles to [[internal link]]
+    for line in Title:
+        mdTitle.append("[["+line+"]] ")
+    
+    return mdTitle
+
+
+def convertInternalLink(matchObj):
+# converts Notion Internal links (found by regex) to Obsidian pretty links
+
+    regexSymbols = "[^\w\s]"
+    regexSpaces = "\s+"
+
+    InternalTitle = matchObj.group(1)
+    ExternalURL = matchObj.group(2)
+    urlTitle = matchObj.group(3)
+    # print("Groups :",match.groups())
+    
+    # Replace symbols with space
+    InternalLink = re.sub(regexSymbols," ",InternalTitle)
+    urlTitle = re.sub(regexSymbols," ",urlTitle)
+    
+    # Remove duplicate spaces
+    InternalLink = re.sub(regexSpaces, " ", InternalLink)
+    urlTitle = re.sub(regexSpaces," ",urlTitle)
+    
+    # Remove any spaces at beginning
+    InternalLink = InternalLink.lstrip()
+    
+    # Cut title at 50 characters
+    InternalLink = InternalLink[0:50]
+    urlTitle = urlTitle[0:50]
+    
+    # Remove any spaces at end
+    InternalLink = InternalLink.rstrip()
+    urlTitle = urlTitle.rstrip()
+
+    # Switch Notion Internal Link for Obsidian Internal Link
+    
+    # URL page title supercedes Internal Link name
+    if InternalLink != urlTitle:
+        InternalLink = urlTitle
+        
+    # Reconstruct Internal Links as pretty links and source footnote
+    if InternalLink == InternalTitle:
+        PrettyLink = "[["+InternalLink+"]] ^["+ExternalURL+"] "
+    else:
+        PrettyLink = "[["+InternalLink+"|"+InternalTitle+"]] ^["+ExternalURL+"] "
+
+    # Substitute regex find with PrettyLink
+    return PrettyLink
+
+def convertBlankLink(matchObj):
+# converts Notion about:blank links (found by regex) to Obsidian pretty links
+
+    regexSymbols = "[^\w\s]"
+    regexSpaces = "\s+"
+    
+    InternalTitle = matchObj.group(1)
+    
+    # Replace symbols with space
+    InternalLink = re.sub(regexSymbols," ",InternalTitle)
+    
+    # Remove duplicate spaces
+    InternalLink = re.sub(regexSpaces, " ", InternalLink)
+    
+    # Remove any spaces at beginning
+    InternalLink = InternalLink.lstrip()
+    
+    # Cut title at 50 characters
+    InternalLink = InternalLink[0:50]
+    
+    # Remove any spaces at end
+    InternalLink = InternalLink.rstrip()
+    
+    # Reconstruct Internal Links as pretty links
+    PrettyLink = "[["+InternalLink+"]] "
+    
+    # Substitute regex find with PrettyLink
+    return PrettyLink
+    
+    
+    
+
+def N2Omd(mdFile):
+    # Local Dependancies: convertInternalLink(), convertBlankLink()
+    
+    newLines = []
+    
+    for line in mdFile:
+        line = line.decode("utf-8").rstrip()
+    
+  
+        
+  
+    # folder style links
+        regexPath =     "^\[(.+)\]\(([^\(]*)(?:\.md|\.csv)\)$" 
+        regexUID =      "%20\w{32}"
+        regex20 =       "%20"
+        regexSlash =    "\s\/"
+        
+        # Identify and group relative paths
+        pathMatch = re.search(regexPath,line)
+        # modify paths into local links. just remove UID and convert spaces
+        if pathMatch:
+            Title = pathMatch.group(1)
+            relativePath = pathMatch.group(2)
+            # Clean UID
+            relativePath = re.sub(regexUID," ",relativePath)
+            # correct spaces
+            relativePath = re.sub(regex20," ",relativePath)
+            relativePath = re.sub(regexSlash,"/",relativePath).strip()
+            
+            # Reconstruct Links as pretty links
+            PrettyLink = "[["+relativePath+"|"+Title+"]] "
+
+            line = PrettyLink
+            # print(line)
+        
+        
+        
+        
+        
+        # Internal style links. 
+        ## Group1:Pretty Link Title 
+        ## Group2: URL. 
+        ## Group3: target file name (in web form but not in exported form without symbols) 
+        regexInternalLink = "\[(.[^\[\]\(\)]*)\]\((https:\/\/www.notion.so\/(?:.[^\/]*)\/(.[^\[\]\(\)]*)-.[^\[\]\(\)]*)\)"
+        
+        match = re.search(regexInternalLink,line)
+        # Substitute regex find with PrettyLink
+        if match:
+            line = re.sub(regexInternalLink, convertInternalLink, line)       
+
+        
+        
+        
+        
+        # about:blank links (lost or missing links within Notion)
+        ## Group1:Pretty Link Title
+        regexBlankLink = "\[(.[^\[\]\(\)]*)\]\(about:blank#.[^\[\]\(\)]*\)"
+        
+        matchBlank = re.search(regexBlankLink,line) 
+        if matchBlank:
+            line = re.sub(regexBlankLink, convertBlankLink, line)
+        
+        
+
+        
+        
+        # Embedded attachment links
+        regexAttached = "!\[(.[^\[\]\(\)]*)\]\((.[^\[\]\(\)]*)\)"
+        regexUID =      "%20\w{32}"
+        regex20 =       "%20"
+        regexSlash =    "\s\/"
+
+        matchAttach = re.search(regexAttached,line)
+        if matchAttach:
+            attachment = matchAttach.group(1)
+            # Clean UID
+            attachment = re.sub(regexUID," ",attachment)
+            # correct spaces
+            attachment = re.sub(regex20," ",attachment)
+            attachment = re.sub(regexSlash,"/",attachment).strip()
+            
+            # Reconstruct Links as embedded links
+            embededLink = "![["+attachment+"]] "
+
+            line = re.sub(regexAttached, embededLink, line)        
+            # print(line)        
+        
+        
+        # Convert tags after lines starting with "Tags:"
+        regexTags = "^Tags:\s(.+)"
+        
+         # Search for Internal Links. Will give match.group(1) & match.group(2)
+        tagMatch = re.search(regexTags,line)
+        
+        Otags = []
+        if tagMatch:
+            Ntags = tagMatch.group(1).split(",")
+            for t in enumerate(Ntags):
+                Otags.append("#"+t[1].strip())
+
+            line = "Tags: "+", ".join(Otags)
+            # print(line)
+    
+
+
+
+        newLines.append(line)
+    
+    return newLines
+
+    
+    
